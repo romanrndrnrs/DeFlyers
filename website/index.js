0c6023289e4ce74e1485d9b058fa3f9bafd8501d
romanrndrnrs <roman.randri@protonmail.com>
2022-04-15 20:04:02 +0200

    Add images to database

--- website/index.js
+++ website/index.js
@@ -1,12 +1,30 @@
 const express = require("express");
 const app = express();
 const port = "3000";
+const fs = require("fs");
+const formidable = require("formidable");
+const path = require("path");
 app.use(express.static('public'));
 
 
 // app.get('/', (req,res) => {
 //     res.send("Hello world!");
 // })
+
+app.post('/fichier', (request,response) =>{
+    var form = new formidable.IncomingForm();
+    form.parse(request, function (err, fields, files) {
+//        console.log(files);
+          var oldpath = files.fileImage.filepath;
+          var newpath = path.join(__dirname + '/public/images',  files.fileImage.newFilename + files.fileImage.originalFilename);
+          fs.copyFile(oldpath, newpath, function (err) {
+                if (err) throw err;
+                response.write(files.fileImage.newFilename + files.fileImage.originalFilename);
+                response.end();
+          });
+    
+    });
+})
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`)
